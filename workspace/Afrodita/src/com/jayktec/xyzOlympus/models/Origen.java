46a0a4fce64a04f9b255ff160162d4a235c3234a
yisheng <yileone@gmail.com>
2018-04-04 06:57:13 -0400

    origen corregido

--- workspace/Afrodita/src/com/jayktec/xyzOlympus/models/Origen.java
+++ workspace/Afrodita/src/com/jayktec/xyzOlympus/models/Origen.java
@@ -1,7 +1,5 @@
 package com.jayktec.xyzOlympus.models;
 
-
-
 import java.sql.*;
 import java.util.Collection;
 import java.util.Date;
@@ -15,149 +13,122 @@ import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
 
 import com.jayktec.persistencia.*;
- 
-
 
 @Entity
-@View(name="VOrigenenUmbral",members="Orige {#nombre,modelo;mac,ip; sistema,tipo;} DIRECCION {#pais,municipio,sucursal,oficina,ciudad}" )
-@Table(name="fateon_origen")
+@View(name = "VOrigenenUmbral", members = "Orige {#nombre,modelo;mac,ip; sistema,tipo;} DIRECCION {#pais,municipio,sucursal,oficina,ciudad}")
+@Table(name = "fateon_origen")
 public class Origen {
-	
+
 	@Id
-	@GeneratedValue(generator="system-uuid")	
+	@GeneratedValue(generator = "system-uuid")
 	@Hidden
-	@GenericGenerator(name="system-uuid",strategy="uuid")
-	@Column(name="origen_id",length=32)
-	private String oid; 
-	
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	@Column(name = "origen_id", length = 32)
+	private String oid;
+
 	@Required
-	@Column(name="origen_nombre",length=45)
-	private String nombre;	
-	
-	//@Required
-	@Column(name="origen_modelo",length=45)
+	@Column(name = "origen_nombre", length = 45)
+	private String nombre;
+
+	// @Required
+	@Column(name = "origen_modelo", length = 45)
 	private String modelo;
+
+	// @Required
+	@Column(name = "origen_mac", length = 100)
+	private String mac;
+
+	// @Required
+	@Column(name = "hora_apertura", length = 9)
+	@Pattern(regexp = "^(?:(?:([01]?\\d|2[0-3]):)?([0-5]?\\d):)?([0-5]?\\d)$", message = "Formato de Hora invalido")
+	@Stereotype("HORA_MINUTO")
 	
-	//@Required
-	@Column(name="origen_mac",length=100)
-	private String mac;	
+	private Timestamp horaApertura;
+
+	// @Required
+	@Column(name = "hora_cierre", length = 9)
+	@Pattern(regexp = "^(?:(?:([01]?\\d|2[0-3]):)?([0-5]?\\d):)?([0-5]?\\d)$", message = "Formato de Hora invalido")
+	@Stereotype("HORA_MINUTO")
 	
-	//@Required
-		@Column(name="hora_apertura",length=9)
-		@Stereotype("HORAS")
+	private Timestamp horaCierre;
 
-		private Time  horaApertura ;	
-		
-
-		//@Required
-			@Column(name="hora_cierre",length=9)
-			@Stereotype("HORAS")
-
-			private Time  horaCierre ;	
-				
-	//@Required
-	@Column(name="origen_ip",length=15,nullable = true) 
-    @Pattern(regexp="^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +// Se valida la direccion IP 
-            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$",
-        message = "Direccion IP Ivalida") 
+	// @Required
+	@Column(name = "origen_ip", length = 15, nullable = true)
+	@Pattern(regexp = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." + // Se valida la direccion IP
+			"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+			+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$", message = "Direccion IP Ivalida")
 	private String ip;
-	
 
-
-
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_tipo",insertable=true,updatable=true,table="fateon_origen")
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Tipo_Sistemas')",
-			showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_tipo", insertable = true, updatable = true, table = "fateon_origen")
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Tipo_Sistemas')", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo tipo;
-	
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_sistema",insertable=true,updatable=true,table="fateon_origen")
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Sistemas')",
-			showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_sistema", insertable = true, updatable = true, table = "fateon_origen")
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Sistemas')", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo sistema;
-	
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_pais",insertable=true,updatable=true)
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Paises')",
-			showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_pais", insertable = true, updatable = true)
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Paises')", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo pais;
-	
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_provincia",insertable=true,updatable=true)
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Provincias' )",
-		showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_provincia", insertable = true, updatable = true)
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Provincias' )", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo provincia;
-		
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_sucursal",insertable=true,updatable=true)
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Sucursales')",
-			showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_sucursal", insertable = true, updatable = true)
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Sucursales')", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo sucursal;
-	
-	
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_oficina",insertable=true,updatable=true)
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Oficinas')",
-			showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_oficina", insertable = true, updatable = true)
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Oficinas')", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo oficina;
-	
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_municipio",insertable=true,updatable=true)
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Municipios')",
-			showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_municipio", insertable = true, updatable = true)
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Municipios')", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo municipio;
 
-
-	
-	//@NoModify
-	//@NoCreate
-	@ManyToOne(fetch=FetchType.LAZY,optional=false)
-	@JoinColumn(name="origen_id_ciudad",insertable=true,updatable=true)
-	@DescriptionsList(condition="e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Ciudades')",
-			showReferenceView=false,
-			descriptionProperties="valorCadena")  
-	@ReferenceView("VVarchar") 
+	// @NoModify
+	// @NoCreate
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "origen_id_ciudad", insertable = true, updatable = true)
+	@DescriptionsList(condition = "e.oid in( SELECT c.oid FROM Catalogo c, Tabla t where c.tabla = t.oid and t.nombre ='Ciudades')", showReferenceView = false, descriptionProperties = "valorCadena")
+	@ReferenceView("VVarchar")
 	private Catalogo ciudad;
-	
+
 	@NoModify
 	@NoCreate
-	@OneToMany(mappedBy="origen")
+	@OneToMany(mappedBy = "origen")
 	@CollectionView("VUmbralenOrigen")
 	private Collection<Umbral> umbral;
 
-
-	
 	public Origen(String string) {
 		// TODO Auto-generated constructor stub
 		buscarOrigen(string);
@@ -165,7 +136,7 @@ public class Origen {
 
 	public Origen() {
 	}
-	
+
 	/**
 	 * genera todo un origen busncandolo de bd a partir del id
 	 * 
@@ -195,57 +166,53 @@ public class Origen {
 		return ciudad;
 	}
 
-
 	public void setCiudad(Catalogo ciudad) {
 		this.ciudad = ciudad;
 	}
-
 
 	public String getOid() {
 		return oid;
 	}
 
-
 	public void setOid(String oid) {
 		this.oid = oid;
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
 	/**
 	 * @return the horaApertura
 	 */
-	public Time getHoraApertura() {
+	public Timestamp getHoraApertura() {
 		return horaApertura;
 	}
 
 	/**
-	 * @param horaApertura the horaApertura to set
+	 * @param horaApertura
+	 *            the horaApertura to set
 	 */
-	public void setHoraApertura(Time horaApertura) {
+	public void setHoraApertura(Timestamp horaApertura) {
 		this.horaApertura = horaApertura;
 	}
 
 	/**
 	 * @return the horaCierre
 	 */
-	public Time getHoraCierre() {
+	public Timestamp getHoraCierre() {
 		return horaCierre;
 	}
 
 	/**
-	 * @param horaCierre the horaCierre to set
+	 * @param horaCierre
+	 *            the horaCierre to set
 	 */
-	public void setHoraCierre(Time horaCierre) {
+	public void setHoraCierre(Timestamp horaCierre) {
 		this.horaCierre = horaCierre;
 	}
 
@@ -253,88 +220,69 @@ public class Origen {
 		return modelo;
 	}
 
-
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
-
 
 	public String getMac() {
 		return mac;
 	}
 
-
 	public void setMac(String mac) {
 		this.mac = mac;
 	}
-
 
 	public String getIp() {
 		return ip;
 	}
 
-
 	public void setIp(String ip) {
 		this.ip = ip;
 	}
-
-
-
 
 	public Catalogo getSistema() {
 		return sistema;
 	}
 
-
 	public void setSistema(Catalogo sistema) {
 		this.sistema = sistema;
 	}
-
 
 	public Catalogo getTipo() {
 		return tipo;
 	}
 
-
 	public void setTipo(Catalogo tipo) {
 		this.tipo = tipo;
 	}
-
 
 	public Catalogo getSucursal() {
 		return sucursal;
 	}
 
-
 	public void setSucursal(Catalogo sucursal) {
 		this.sucursal = sucursal;
 	}
-
 
 	public Catalogo getOficina() {
 		return oficina;
 	}
 
-
 	public void setOficina(Catalogo oficina) {
 		this.oficina = oficina;
 	}
-
 
 	public Catalogo getMunicipio() {
 		return municipio;
 	}
 
-
 	public void setMunicipio(Catalogo municipio) {
 		this.municipio = municipio;
 	}
 
-
 	public Catalogo getPais() {
 		return pais;
 	}
-
 
 	public void setPais(Catalogo pais) {
 		this.pais = pais;
@@ -356,6 +304,4 @@ public class Origen {
 		this.umbral = umbral;
 	}
 
-
 }
-
