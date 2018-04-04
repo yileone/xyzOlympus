298060b1c5f563799f01d6b2b5bd839f001dc14b
yisheng <yileone@gmail.com>
2018-04-04 12:51:43 -0400

    corrigiendo campo en consulta

--- workspace/Afrodita/src/com/jayktec/traductor/ToJson.java
+++ workspace/Afrodita/src/com/jayktec/traductor/ToJson.java
@@ -89,7 +89,7 @@ public class ToJson {
 
 	private static String postCategoria = "}";// ", \"stepSkipped\": false,
 	private static ArrayList<Umbral> listaUmbrales;
-	private static boolean habil=false;
+	private  boolean habil = true;
 	// \"appliedSmartLabel\": true
 	// }";
 
@@ -108,7 +108,7 @@ public class ToJson {
 	}
 
 	public ToJson(String origen, String sensor, boolean habil) throws SQLException {
-		this(origen,sensor);
+		this(origen, sensor);
 		setHabil(habil);
 	}
 
@@ -120,7 +120,7 @@ public class ToJson {
 	}
 
 	public ToJson(String origen, Sensor sensor, boolean habil) throws SQLException {
-		this(origen,sensor);
+		this(origen, sensor);
 		setHabil(habil);
 	}
 
@@ -687,7 +687,7 @@ public class ToJson {
 	 */
 	private void buscarMediaMovil() throws SQLException {
 		listaMedia = null;
-		listaMedia = BdManager.buscarMediaMovil(sensor, origen,habil);
+		listaMedia = BdManager.buscarMediaMovil(sensor, origen, habil);
 
 	}
 
@@ -710,109 +710,12 @@ public class ToJson {
 		crearCategorias();
 		creaDataset();
 		return encabezado + categorias + dataset;
-		// String temp = "{";
-		//
-		// for (Registro registro : listaRegistro) {
-		// temp = temp + "{";
-		// for (Mapa mapaItem : mapa) {
-		// String campo = mapaItem.getMapabdToString();
-		// String valor = "";
-		// String nombreCampo = mapaItem.getMapaapp();
-		// if (campo.equals(Constantes.CampoRegistro.DATE1.campoBD())) {
-		// valor = registro.getRegistroDate1().toString();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.DATE2.campoBD())) {
-		// valor = registro.getRegistroDate2().toString();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.DATE3.campoBD())) {
-		// valor = registro.getRegistroDate3().toString();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.DATE4.campoBD())) {
-		// valor = registro.getRegistroDate4().toString();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.DATE5.campoBD())) {
-		// valor = registro.getRegistroDate5().toString();
-		// }
-		//
-		// if (campo.equals(Constantes.CampoRegistro.INT1.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroInt1());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.INT2.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroInt2());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.INT3.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroInt3());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.INT4.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroInt4());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.INT5.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroInt5());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.FLOAT1.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroFloat1());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.FLOAT2.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroFloat2());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.FLOAT3.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroFloat3());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.FLOAT4.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroFloat4());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.FLOAT5.campoBD())) {
-		// valor = String.valueOf(registro.getRegistroFloat5());
-		// }
-		//
-		// if (campo.equals(Constantes.CampoRegistro.VARCHAR1.campoBD())) {
-		// valor = registro.getRegistroVarchar1();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.VARCHAR2.campoBD())) {
-		// valor = registro.getRegistroVarchar2();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.VARCHAR3.campoBD())) {
-		// valor = registro.getRegistroVarchar3();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.VARCHAR4.campoBD())) {
-		// valor = registro.getRegistroVarchar4();
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.VARCHAR5.campoBD())) {
-		// valor = registro.getRegistroVarchar5();
-		// }
-		//
-		// if (campo.equals(Constantes.CampoRegistro.HORA1.campoBD())) {
-		// valor = String.valueOf(registro.getRegistrotime1());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.HORA2.campoBD())) {
-		// valor = String.valueOf(registro.getRegistrotime2());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.HORA3.campoBD())) {
-		// valor = String.valueOf(registro.getRegistrotime3());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.HORA4.campoBD())) {
-		// valor = String.valueOf(registro.getRegistrotime4());
-		// }
-		// if (campo.equals(Constantes.CampoRegistro.HORA5.campoBD())) {
-		// valor = String.valueOf(registro.getRegistrotime5());
-		// }
-		//
-		// temp = temp + '\n' + '\"' + nombreCampo + '\"' + ":" + '\"' + valor +
-		// '\"';
-		//
-		// }
-		//
-		// temp = temp + "}" + '\n';
-		//
-		// }
-		// temp = temp + "}" + '\n';
-		// return temp;
 	}
 
 	private void buscarRegistros() throws SQLException {
 		// TODO Auto-generated method stub
 		ToJson.listaRegistro = null;
-		ToJson.listaRegistro = BdManager.consultarRegistro(origen, sensor,habil);
+		ToJson.listaRegistro = BdManager.consultarRegistro(origen, sensor, habil);
 
 	}
 
@@ -1598,15 +1501,16 @@ public class ToJson {
 	/**
 	 * @return the habil
 	 */
-	public static boolean isHabil() {
+	public  boolean isHabil() {
 		return habil;
 	}
 
 	/**
-	 * @param habil the habil to set
+	 * @param habil
+	 *            the habil to set
 	 */
-	public static void setHabil(boolean habil) {
-		ToJson.habil = habil;
+	public  void setHabil(boolean habil) {
+		this.habil = habil;
 	}
 
 }
